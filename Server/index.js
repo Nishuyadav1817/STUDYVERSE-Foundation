11c5fbb3853b19a58cf5433a9a282969e8cc8345
nishuya <prathamyadav1817@gmail.com>
2026-04-19 21:34:35 +0530

    backend url chnage

--- Server/index.js
+++ Server/index.js
@@ -13,7 +13,7 @@ const Redis=require("./redis");
 const cors = require('cors'); 
 
 const allowedOrigins = [
-  "https://tiesverse-foundation-1yk1.vercel.app"
+  "https://studyverse-foundation.vercel.app/"
 
  
 ];
